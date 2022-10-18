9767a5de1e952e0c5857c68bdddead770929cd4d
Julian Sech <info@qsamax.org>
2022-10-18 12:48:08 +0200

    adding POJOs. Closes #2.

--- src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,4 +1,14 @@
 package guru.springframework.sfgpetclinic.model;/* 18/10/2022" "11:55" "Julian */
 
 public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
